ce1bf7cf8179d74375be7ad18579471e150e38a5
vittoriaromano <vittoria5005@gmail.com>
2023-03-06 21:59:02 +0400

    array-fix

--- index.js
+++ index.js
@@ -4,16 +4,15 @@
 // funzione prompt  
 function prmpttNumber(){
      //array of Usernumb  - con il ciclo non mi funziona
-    numberUser = [Number (prompt ('inserisci un numero visualizato')),
-     Number (prompt ('inserisci un numero visualizato')),
-     Number (prompt ('inserisci un numero visualizato')),
-     Number (prompt ('inserisci un numero visualizato')),
-     Number (prompt ('inserisci un numero visualizato')),]
+     for (let i=0; i <5; i++){
+        numberUser.push(Number (prompt ('inserisci un numero visualizato')));
 
+    }
+     
     console.log(numberUser);
     //comparare le due
     let numbersEql = [];
-    numbersEql  = arrRandom.filter((data) => numberUser.includes(data));
+    numbersEql  = arrRandom.filter((data) => numberUser.includes(data));//invece dell' if value.includes value2[i]
     console.log(numbersEql);
     impNumb.innerHTML = ('Ti sei ricordato questi numeri; ' + numbersEql + '!')
 
@@ -27,6 +26,8 @@ function numerarr(){
 //MAIN
 let numberUser = [];
 
+
+
 setTimeout(prmpttNumber, 30000); //timer 30s prompt
 
 setTimeout(numerarr, 29000); // timer 2.9 s rimuover arr A
@@ -34,7 +35,13 @@ setTimeout(numerarr, 29000); // timer 2.9 s rimuover arr A
 
 //creare un array di numeri random che verranno visualizati nel dom. (A)
 
-const arrRandom = [2,4,6,3,5];
+const arrRandom = [];
+
+for(let i = 0; i < 5; i++){
+    const num = Math.floor((Math.random() * 10) + 1);
+    arrRandom.push(num);
+}
+
 let impNumb = document.getElementById('imp-numb');
 impNumb.innerHTML = arrRandom;
 console.log(arrRandom)
